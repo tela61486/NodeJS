e5467c55b75619a5678af635cbd6a9d68de56b8d
brandon thomas <Tela@brandons-Air.attlocal.net>
2018-08-06 15:57:42 -0500

    SocketIO is listening to the HTTP server, that listening on port 4001

--- SocketIOApps/TemperatureApp/app.js
+++ SocketIOApps/TemperatureApp/app.js
@@ -1,11 +1,19 @@
+const port = process.env.PORT || 4001;
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 
+// Setups the socketIO to listen to the server
+// for socket connections
 const socketio= require("socket.io").listen(server);
 
 const axios = require("axios");
 
-const port = process.env.PORT || 4001;
+
 const index = require("./routes/index");
 
+app.use(index);
+
+const getApiAndEmit = "TODO"
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
